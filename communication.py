aac9698a66b37dee0299c5b2b8bd9e43035e92da
Shanx-liu <sam0912817075@gmail.com>
2026-06-04 19:09:59 +0800

    **建立還原點**   修改內容：將sent_time回傳型別變為str

--- communication.py
+++ communication.py
@@ -9,34 +9,36 @@ class Action(BaseModel):
     target_node_id: str
     input_text: str | None
 
+#以下為傳給前端之 JSON 定義
+
 class AskMessage(BaseModel):
     """詢問使用者任務具體細節"""
     type: str
     ask_messages: str
-    sent_time: datetime      #傳送json當下的時間
+    sent_time: str      #傳送json當下的時間
 
 class TaskStartMessage(BaseModel):
     """通知APP端任務開始"""
     type: str
-    sent_time: datetime
+    sent_time: str
 
 class ReadUIAndScreenshot(BaseModel):
     """告知APP讀取UI樹與截圖"""
     type: str
-    sent_time: datetime
+    sent_time: str
 
 class ActionCheck(BaseModel):
     """通知使用者敏感操作需確認"""
     type: str
     action_detail: str
     sensitive_reason: str
-    sent_time: datetime
+    sent_time: str
 
 class OperateCommand(BaseModel):
     """發送操作指令給APP"""
     type: str
     current_action: Action
-    sent_time: datetime
+    sent_time: str
 
 class TaskEndMessages(BaseModel):
     """關閉APP進程、告知使用者執行結果"""
@@ -44,9 +46,9 @@ class TaskEndMessages(BaseModel):
     task_result: str
     task_process: str    
     error_reason: str
-    sent_time: datetime
+    sent_time: str
 
-def now_timestamp() -> datetime:
-    """回傳目前時間的函式"""
+def now_timestamp() -> str:
+    """回傳當下時間的函式，回傳型別為 str"""
     tz = zoneinfo.ZoneInfo("Asia/Taipei")
-    return datetime.now(tz)
+    return datetime.now(tz).isoformat()
